f4373ec4116b565a49c831fb1b7ef47a760e7b97
hebert.silva <hebert.silva@cibus.app.br>
2023-08-28 21:27:02 -0300

    first commit

---
+++ src/main/java/com/hebert/proximitychatappapi/ProximityChatAppApiApplication.java
@@ -0,0 +1,13 @@
+package com.hebert.proximitychatappapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProximityChatAppApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProximityChatAppApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hebert/proximitychatappapi/ProximityChatAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hebert.proximitychatappapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProximityChatAppApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
